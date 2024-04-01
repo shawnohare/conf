b687195a59b994e69e4385ce219f70e15c890a0d
Shawn O'Hare <shawn@shawnohare.com>
2024-04-01 09:32:15 -0700

    Add autopairing to ipython config.

--- home/etc/ipython/config.py
+++ home/etc/ipython/config.py
@@ -1,10 +1,13 @@
 # Configuration file for ipython.
 #
 # To generate a new config to see, e.g., all the options, run
-# ipython profile generate foo
+# ipython profile create foo
 # from ipython.config import Config
 # c = Config()
+c = get_config()  #noqa
 
+c.TerminalInteractiveShell.auto_match = True
 c.TerminalInteractiveShell.editor = "nvim"
 c.TerminalInteractiveShell.editing_mode = "vi"
-c.Completer.use_jedi = True
+c.TerminalInteractiveShell.true_color = True
+# c.Completer.use_jedi = True
